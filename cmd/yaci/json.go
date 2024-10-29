6ac28f9f4346c39aabe8b06a8e147d20420770e1
Félix C. Morency <1102868+fmorency@users.noreply.github.com>
2024-10-29 08:39:09 -0400

    feat: add viper support to cobra (#10)

--- cmd/yaci/json.go
+++ cmd/yaci/json.go
@@ -1,21 +1,24 @@
 package yaci
 
 import (
+	"log/slog"
 	"os"
 
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 
 	"github.com/liftedinit/yaci/internal/output"
 )
-
-var jsonOut string
 
 var jsonCmd = &cobra.Command{
 	Use:   "json [address] [flags]",
 	Short: "Extract chain data to JSON files",
 	Args:  cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
+		jsonOut := viper.GetString("json-out")
+		slog.Debug("Command-line argument", "json-out", jsonOut)
+
 		err := os.MkdirAll(jsonOut, 0755)
 		if err != nil {
 			return errors.WithMessage(err, "failed to create output directory")
@@ -32,5 +35,8 @@ var jsonCmd = &cobra.Command{
 }
 
 func init() {
-	jsonCmd.Flags().StringVarP(&jsonOut, "out", "o", "out", "Output directory")
+	jsonCmd.Flags().StringP("json-out", "o", "out", "JSON output directory")
+	if err := viper.BindPFlags(jsonCmd.Flags()); err != nil {
+		slog.Error("Failed to bind jsonCmd flags", "error", err)
+	}
 }
