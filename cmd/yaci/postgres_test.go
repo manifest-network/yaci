7057fa31d19a381137cac336685dd6107b8def48
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2025-01-22 11:20:57 -0500

    fix: e2e test

--- cmd/yaci/postgres_test.go
+++ cmd/yaci/postgres_test.go
@@ -60,8 +60,8 @@ func testExtractBlocksAndTxs(t *testing.T) {
 
 		transactions := getJSONResponse(t, RestTxEndpoint, nil)
 		require.NotEmpty(t, transactions)
-		// The number of transactions is 6 as defined in the `infra.yml` file under the `manifest-ledger-tx` service
-		require.Len(t, transactions, 6)
+		// The number of transactions is 25 as defined in the `infra.yml` file under the `manifest-ledger-tx` service
+		require.Len(t, transactions, 25)
 	})
 }
 
