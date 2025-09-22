102714f787ccf395f28e5a07694e047dc44ef6a9
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2025-09-22 14:05:42 -0400

    fix: use events to collect burn and payout

--- internal/metrics/collectors/total_payout_burn.go
+++ internal/metrics/collectors/total_payout_burn.go
@@ -10,21 +10,7 @@ import (
 
 // TotalPayoutBurnQuery expected format of the cosmwasm event attribute for burn amounts, e.g., 123456umfx
 const TotalPayoutBurnQuery = `
-		WITH payout_amount AS (
-		  SELECT
-			COALESCE(SUM((pp.element->'coin'->>'amount')::numeric), 0) AS amount
-		  FROM api.messages_main m
-		  CROSS JOIN LATERAL jsonb_array_elements(m.metadata->'payoutPairs') AS pp(element)
-		  WHERE m.type = '/liftedinit.manifest.v1.MsgPayout'
-		),
-		burn_amount AS (
-		  SELECT
-			COALESCE(SUM((pp.element->>'amount')::numeric), 0) AS amount
-		  FROM api.messages_main m
-		  CROSS JOIN LATERAL jsonb_array_elements(m.metadata->'burnCoins') AS pp(element)
-		  WHERE type = '/liftedinit.manifest.v1.MsgBurnHeldBalance'
-		),
-		burn_amount_contract AS (
+		WITH mfx_burned AS (
 		  WITH burn_events AS (
 		    SELECT
 		  	e.id,
@@ -40,16 +26,36 @@ const TotalPayoutBurnQuery = `
 		  JOIN api.messages_main m
 		    ON m.id = b.id
 		   AND m.message_index = b.msg_index
-		   AND m.type = '/cosmwasm.wasm.v1.MsgExecuteContract'
 		  JOIN LATERAL regexp_matches(
 		    b.amount_raw,
 		    '^([0-9]+)([[:alnum:]_\/\.]+)$'
 		  ) AS rm(captures) ON TRUE
 		  WHERE (rm.captures)[2] = 'umfx'
-	    )
-		SELECT 
-		  (SELECT amount FROM payout_amount) AS payout_amount,
-		  (SELECT amount FROM burn_amount) + (SELECT amount FROM burn_amount_contract) AS burn_amount
+        ),
+		payout_total AS (
+          WITH payout_events AS (
+			SELECT
+		  	e.id,
+		  	e.event_index,
+		  	MAX(CASE WHEN e.attr_key = 'payout_amount' THEN e.attr_value END) AS amount_raw,
+		  	MAX(e.msg_index) AS msg_index
+		    FROM api.events_main e
+		    WHERE e.event_type = 'payout'
+		    GROUP BY e.id, e.event_index
+		  )
+		  SELECT COALESCE(SUM((rm.captures)[1]::numeric), 0) AS amount
+		  FROM payout_events b
+		  JOIN api.messages_main m
+		    ON m.id = b.id
+		   AND m.message_index = b.msg_index
+		  JOIN LATERAL regexp_matches(
+		    b.amount_raw,
+		    '^([0-9]+)([[:alnum:]_\/\.]+)$'
+		  ) AS rm(captures) ON TRUE
+		  WHERE (rm.captures)[2] = 'umfx'
+		)
+		SELECT (SELECT amount FROM payout_total),
+		       (SELECT amount FROM mfx_burned);
 	`
 
 type TotalPayoutBurnCollector struct {
